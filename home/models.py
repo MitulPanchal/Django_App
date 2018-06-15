8fe860f9fdd6fec8c6556f8028e5a65674c24be5
MitulPanchal <imitulpanchal@gmail.com>
2018-06-15 21:19:36 +0530

    Start Django App - Learn Database

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee  (models.Model):
+    Name =  models.CharField(max_length=250)
+    Department = models.CharField(max_length=250)
+    Contact = models.CharField(max_length=100)
+
+class Manager(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    type = models.CharField(max_length=100)
+    managerName = models.CharField(max_length=250)
